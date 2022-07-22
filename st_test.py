8bdc36a378e5fe4051cfc607ef51854234f238b3
Samuel Changsam Jeon <61372382+samuel0922@users.noreply.github.com>
2022-07-22 16:24:26 +0900

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-from st_aggrid import AgGrid
+#from st_aggrid import AgGrid
 
 # import matplotlib.pyplot as plt
 # import matplotlib
